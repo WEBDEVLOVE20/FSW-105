098c73dfad286ccebc19a05ea27e428ae83a80cc
WEBDEVLOVE20 <60906754+WEBDEVLOVE20@users.noreply.github.com>
2020-02-28 18:13:30 -0500

    for-loops

---
+++ week3/functions/index.js
@@ -0,0 +1,34 @@
+function sum(nmbr1, nmbr2){
+    return nmbr1 + nmbr2
+}
+
+var result = sum(25, 25)
+console.log(result);
+
+
+
+function highestNum(nmbr1, nmbr2, nmbr3){
+
+}
+
+console.log(Math.max(10, 20, 30));
+
+
+function oneNum(nmbr1){
+
+    if(nmbr1 % 2 === 0){
+        console.log("even")
+    } else {
+        console.log("odd")
+    } 
+}
+console.log(oneNum(7));
+
+/*
+function str(){
+    var charct = str("How are you doing today?")
+    if(charct.length <= 25){
+}
+}
+console.log()
+*/
---
+++ week3/loops_and_arrays/index.js
@@ -0,0 +1,98 @@
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"];
+
+var numOfComputer = 0;
+
+for(var i = 0; i < officeItems.length; i++){
+    if(officeItems[i] === "computer") {
+        numOfComputer++;
+    }
+}
+    console.log(numOfComputer);
+
+
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    },{
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    },{
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    },{
+        name: "Sam",
+        age: 30,
+        gender: "male"
+    },{
+        name: "Suzy",
+        age: 4,
+        gender: "female"
+    }
+];
+
+for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+    if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+        console.log("old enough");
+     } else {
+         console.log("not old enough");
+        }
+}
+
+
+for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+    
+    if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough to see Mad Max.");
+    } else {
+         console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max.");
+        }
+}
+
+
+for(var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+var genderReview = (peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female") ? "HER" : "HIM";
+    if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough to see Mad Max Fury Road let " + genderReview + " in.");
+        } else {
+         console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max Fury Road, don't let " + genderReview + " in.");
+       }  
+     }
+
+
+
+for(var i = 0; i < 101; i++){
+    if([i] % 2 === 0){
+        console.log([i] + " Even"); //I can remove [i] from console.log to remove number and leave only string, figured it was easier to read this way though.
+    } else {
+        console.log([i] + " Odd");
+    }
+}
+
+
+/*
+var arrayOfArrays = [
+[2, 5, 3, 4, 3],
+[1, 1, 1, 1, 3],
+[9, 3, 4, 2]
+
+]
+
+for(var i = 0; i < arrayOfArrays.length; i++){
+    for(var j = 0; j < arrayOfArrays[i].length; j++){
+        if(arrayOfArrays[j] % 2 === 0)
+    }
+}
+
+
+
+*/
+
+
+
+
+
