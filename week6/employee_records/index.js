99ec2637ed7a35d9df54f03a0912b431b7e9374a
WEBDEVLOVE20 <60906754+WEBDEVLOVE20@users.noreply.github.com>
2020-03-21 12:44:40 -0400

    #

--- week6/employee_records/index.js
+++ week6/employee_records/index.js
@@ -10,19 +10,19 @@ function Employee (name, jobtitle, salary) {
         console.log(employee3);
     
     }
-
+/*
 class SepEmployeeStatus extends Employee{
     constructor(name, jobtitle, salary, sepstatus){
         super(name, jobtitle, salary)
         this.sepstatus = sepstatus;
     }
-}
+} */
 
 
 
 const employee1 = new Employee("Jim", "Developer", 150000);
 const employee2 = new Employee("Karen", "Admin Assistant", 55000);
-const employee3 = new SepEmployeeStatus("Rocky", "Manager", 98000, "Part Time");
+const employee3 = new Employee("Rocky", "Manager", 98000, "Part Time");
 
 
 
