1db7f593e5cd58de228055033531d00f6fea6744
WEBDEVLOVE20 <60906754+WEBDEVLOVE20@users.noreply.github.com>
2020-03-21 23:55:37 -0400

    #

--- week6/employee_records/index.js
+++ week6/employee_records/index.js
@@ -1,32 +1,23 @@
 const employees = [];
 
-function Employee (name, jobtitle, salary) {
+function Employee (name, jobtitle, salary, status = "Full Time") {
     this.name = name;
     this.jobtitle = jobtitle;
     this.salary = salary;
-    this.status = "Full Time";
-}
-    printEmployeeForm = function(){
-        console.log(employee3);
+    this.status = status;
+    this.printEmployeeForm = function(){
+        console.log(`Name: ${name}, Job Title: ${jobtitle}, Salary: ${salary}/yearly, Status: ${status}`);
     
     }
-/*
-class SepEmployeeStatus extends Employee{
-    constructor(name, jobtitle, salary, sepstatus){
-        super(name, jobtitle, salary)
-        this.sepstatus = sepstatus;
-    }
-} */
+    
+    employees.push(this);
+}
 
+const employee1 = new Employee("Jim", "Developer", 150000,);
+const employee2 = new Employee("Karen", "Admin Assistant", 55000, "Part Time");
+const employee3 = new Employee("Rocky", "Manager", 98000);
 
-
-const employee1 = new Employee("Jim", "Developer", 150000);
-const employee2 = new Employee("Karen", "Admin Assistant", 55000);
-const employee3 = new Employee("Rocky", "Manager", 98000, "Part Time");
-
-
-
-
-printEmployeeForm(employee1)
-printEmployeeForm(employee2)
-printEmployeeForm(employee3) 
+employee1.printEmployeeForm();
+employee2.printEmployeeForm();
+employee3.printEmployeeForm();
+console.log(employees);
